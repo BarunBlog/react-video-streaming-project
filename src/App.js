496bf3a5be96cab38170458a87203b93fba82ede
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-06-25 22:17:27 +0600

    Added the DASH implementation to the project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React, { useEffect } from "react";
+import dashjs from "dashjs";
 import "./App.css";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 function App() {
+  useEffect(() => {
+    let url = API_URL + "/stream-video/";
+    let player = dashjs.MediaPlayer().create();
+    player.initialize(document.querySelector("#videoPlayer"), url, true);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>On Demand Video Streaming Project</p>
-
-        <video controls>
-          <source
-            src="http://127.0.0.1:8000/stream/stream-video/"
-            type="video/mp4"
-          ></source>
-        </video>
+        <video id="videoPlayer" controls></video>
       </header>
     </div>
   );
