51e0882ac0ff5a831d2fbb579929433bfb64ff66
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-07-17 20:44:56 +0600

    Created the Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from '../../api/axios';
+import './home.css';
+import moment from 'moment';
+
+const VIDEO_LIST_URL = '/stream-video/get-videos';
 
 const Home = () => {
-  return <div>This is Homepage</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await axios.get(VIDEO_LIST_URL);
+        setVideos(response.data.results);
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      }
+    };
+
+    fetchVideos();
+  }, [videos]);
+
+  return (
+    <div className="home-container">
+      <div className="videos-grid">
+        {videos.map(video => (
+          <div key={video.uuid} className="video-card">
+            <img src={video.thumbnail} alt={video.title} className="video-thumbnail" />
+            <div className="video-info">
+              <p className="video-title">{video.title}</p>
+
+              <div className="video-author-info">
+                <p className="video-author">Uploaded By {video.author_name}</p>
+                <p className="video-time">{moment(video.created_at).fromNow()}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
